efaae4e0e7802179efb75599a4a5727a086060ea
partha2001ps <parthapn2017@gmail.com>
2023-12-18 21:33:20 +0530

    Done

--- controllers/taskcontroller.js
+++ controllers/taskcontroller.js
@@ -15,32 +15,30 @@ const taskcontroller = {
                 user: userId,
             });
             await newTask.save();
-            res.status(201).json({ message: "Task created successfully", task: newTask });
+            return res.status(201).json({ message: "Task created successfully", task: newTask });
         } catch (error) {
             console.error(error);
-            res.status(500).json({ message: "Internal Server Error" });
+           return res.status(500).json({ message: "Internal Server Error" });
         }
     },
 
     editTask: async (req, res) => {
         try {
             const taskId = req.params.taskId;
-            const { taskTitle,frontendsourcecode, frontendDepoly, backendsourcecode, backendDeploy,commands} = req.body;
+            const {frontendsourcecode, frontendDepoly, backendsourcecode, backendDeploy} = req.body;
             const task = await Task.findById(taskId);
             if (!task) {
                 return res.status(404).json({ message: "Task not found" });
             }
-            task.taskTitle=taskTitle,
             task.frontendsourcecode = frontendsourcecode;
             task.frontendDepoly = frontendDepoly;
             task.backendsourcecode = backendsourcecode;
             task.backendDeploy = backendDeploy;
-            task.commands=commands
             await task.save();
-            res.json({ message: "Task updated successfully", task });
+           return res.json({ message: "Task updated successfully", task });
         } catch (error) {
             console.error(error);
-            res.status(500).json({ message: "Internal Server Error" });
+           return res.status(500).json({ message: "Internal Server Error" });
         }
     },
 
@@ -48,10 +46,10 @@ const taskcontroller = {
         try {
             const userId = req.userId;
             const tasks = await Task.find({ user: userId });
-            res.json({ tasks });
+           return res.json({ tasks });
         } catch (error) {
             console.error(error);
-            res.status(500).json({ message: "Internal Server Error" });
+           return res.status(500).json({ message: "Internal Server Error" });
         }
     },
 };
